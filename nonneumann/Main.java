f5d59004dcd02283e11c9e3b91c7e7b3d56619f6
davidvieira0110 <davidvieira0110@gmail.com>
2025-03-08 20:59:53 -0300

    feat: adicionado algumas produções

--- nonneumann/Main.java
+++ nonneumann/Main.java
@@ -1,26 +1,27 @@
 package nonneumann;
 
 import nonneumann.lexer.Lexer;
-import nonneumann.node.EOF;
-import nonneumann.node.Token;
+import nonneumann.node.Start;
+import nonneumann.parser.*;
+
 import java.io.FileReader;
 import java.io.PushbackReader;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            String arquivo = "C:\\Users\\david\\IdeaProjects\\nonNeumannV2\\nonneumann\\testes\\comentario.non";
+            String arquivo = "C:\\Users\\david\\IdeaProjects\\nonNeumannV2\\nonneumann\\testes_sintatico\\declaracao_funcao.non";
 
-            Lexer lexer =
-                    new Lexer(
-                            new PushbackReader(
-                                    new FileReader(arquivo), 1024));
+            Lexer lex = new Lexer(
+                    new PushbackReader(
+                            new FileReader(arquivo), 1024));
+            Parser p = new Parser(lex);
 
-            Token token;
-            while (!((token = lexer.next()) instanceof EOF)) {
-                System.out.println(token.getClass());
-                System.out.println(" ( " + token + ")");
-            }
+            Start tree = p.parse();
+            //Imprime árvore na saída padrão
+            //tree.apply(new ASTPrinter());
+            //Imprime árvore em interface gráfica
+            tree.apply(new ASTDisplay());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
